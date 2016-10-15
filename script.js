372f2133845cac6d07ec00abae798d3abc188511
litor <litor@192.168.11.5>
2016-10-16 03:53:20 +0900

    スライドショーを実装しました。

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-addEventListener("load", function(){
-	var srcs = [
-		"店舗写真.jpg",
-		"サーロイン皿盛.png"
-	];
-	var img = document.getElementById("cover");
+addEventListener("load", function() {
+	var srcs = [ "cover1.jpg", "cover2.jpg" ];
 	var index = 0;
-	setInterval(function(){
-		index = (index+1) % srcs.length;
-		img.src = srcs[index];
-	}, 5000);
+	var overCover = document.getElementById("overCover");
+	var underCover = document.getElementById("underCover");
+	var animater = $(overCover);
+	setInterval(function() {
+		underCover.src = overCover.src;
+		overCover.style.opacity = 0;
+		index = (index + 1) % srcs.length;
+		overCover.src = srcs[index];
+		animater.animate({
+			opacity : 1.0
+		}, 2000);
+	}, 15000);
 }, false);
