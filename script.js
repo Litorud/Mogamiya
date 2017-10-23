ae18694076019fb19b0f3ee3fc5ce0e0c6e71d97
litor <litor@192.168.1.4>
2017-10-23 19:05:52 +0900

    2017-10-23 必要なコンテンツとスタイルをを一通り適用完了した。

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-addEventListener("load", function() {
-	var srcs = [ "cover1.jpg", "cover2.jpg" ];
+addEventListener("load", function () {
+	var srcs = ["cover1.jpg", "cover2.jpg"];
 	var index = 0;
 	var overCover = document.getElementById("overCover");
 	var underCover = document.getElementById("underCover");
 	var animater = $(overCover);
-	setInterval(function() {
+	setInterval(function () {
 		underCover.src = overCover.src;
 		overCover.style.opacity = 0;
 		index = (index + 1) % srcs.length;
 		overCover.src = srcs[index];
 		animater.animate({
-			opacity : 1.0
+			opacity: 1.0
 		}, 2000);
-	}, 15000);
+	}, 5000);
 }, false);
